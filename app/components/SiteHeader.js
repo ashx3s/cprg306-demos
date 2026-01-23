8ac58d85d144e142a4944943292a9a0179ea71ec
Ashlyn Knox <ashx3s@tuta.io>
2026-01-23 08:31:27 -0700

    Site header refactor

--- app/components/SiteHeader.js
+++ app/components/SiteHeader.js
@@ -1,17 +1,22 @@
 import Link from "next/link";
+import HeaderNavLink from "./HeaderNavLink";
 export default function SiteHeader() {
   return (
     <header className="flex gap-4 justify-between py-4 px-8">
       <div>
+        {/* TODO: Add ICON */}
         <Link href="/">Site Logo</Link>
       </div>
+      {/* TODO: Links to External Resources */}
       <nav>
         <ul className="flex gap-4">
-          <li>
-            <Link href="week-2">Week 2</Link>
-          </li>
-          <li>Link 2</li>
-          <li>Link 3</li>
+          {/* TODO: Decide if we want to make separate components */}
+          <HeaderNavLink href="https://nextjs.org/" text="NextJS Docs" />
+          <HeaderNavLink href="https://tailwindcss.com" text="Tailwind" />
+          <HeaderNavLink
+            href="https://sd-web-2.vercel.app/"
+            text="Course Site"
+          />
         </ul>
       </nav>
     </header>
