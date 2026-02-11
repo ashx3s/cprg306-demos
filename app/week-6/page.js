1b97364ffc898adeb0275ca373c3420aa9bcc9f1
Ashlyn Knox <ashx3s@tuta.io>
2026-02-11 09:54:11 -0700

    form syntax examples

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Page() {
   const [isVisible, setIsVisible] = useState(true);
-
+  const [userName, setUserName] = useState("");
   const num = 30;
 
   function toggleIsVisible() {
@@ -11,12 +11,23 @@ export default function Page() {
       setIsVisible(!isVisible);
     }
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const values = formData.entries();
+    for (const pair of values) {
+      console.log(`Key: ${pair[0]}`, `Value: ${pair[1]}`);
+    }
+    alert(e.target);
+  }
+
   return (
     <main className="mx-8">
       <header>
         <h1>Forms, State, and Interactivity</h1>
       </header>
-      <section>
+      <section className="my-8">
         <button onClick={toggleIsVisible}>Toggle Button</button>
 
         {isVisible && <h2 className="text-2xl font-bold my-4">Hello World</h2>}
@@ -24,6 +35,26 @@ export default function Page() {
         <p className={`${isVisible ? "text-red-500" : "text-blue-500"}`}>
           Check Color Toggle
         </p>
+      </section>
+      <section>
+        <header className="my-2">
+          <h2 className="text-2xl">Form Example</h2>
+          <p>Applying onChange with useState for interactive forms.</p>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="user-name">UserName: </label>
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              type="text"
+              name="userName"
+              value={userName}
+              id="user-name"
+            />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>{userName}</h3>
       </section>
     </main>
   );
