237df5cf4e75c98edd8d48eb47137117eae4e45c
Ashlyn Knox <ashx3s@tuta.io>
2026-02-23 18:07:03 -0700

    homepage fix

--- app/components/DemoNav.js
+++ app/components/DemoNav.js
@@ -12,42 +12,35 @@ const navItems = [
     title: "Week 4",
     href: "week-4",
   },
+  {
+    title: "Week 5",
+    href: "week-5",
+  },
+  {
+    title: "Week 6",
+    href: "week-6",
+  },
+  {
+    title: "Week 7",
+    href: "week-7",
+  },
 ];
 export default function SiteNav() {
   return (
     <ul className="my-4 mx-6 text-2xl">
-      <li className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl">
-        <Link
-          href="week-2"
-          className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear"
+      {navLinks.map((link) => {
+        <li
+          key={link.href}
+          className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl"
         >
-          Week 2
-        </Link>
-      </li>
-      <li className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl">
-        <Link
-          href="week-3"
-          className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear"
-        >
-          Week 3
-        </Link>
-      </li>
-      <li className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl">
-        <Link
-          href="week-4"
-          className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear"
-        >
-          Week 4
-        </Link>
-      </li>
-      <li className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl">
-        <Link
-          href="week-5"
-          className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear"
-        >
-          Week 5
-        </Link>
-      </li>
+          <Link
+            href={link.href}
+            className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear"
+          >
+            {link.title}
+          </Link>
+        </li>;
+      })}
     </ul>
   );
 }
