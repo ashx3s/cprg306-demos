c789ae689d8f2a1b23519ffed0f4527450c40764
Ashlyn Knox <ashx3s@tuta.io>
2026-03-06 08:34:01 -0700

    optimized loading and error state in fetch

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -5,20 +5,27 @@ import { useState, useEffect } from "react";
 export default function Page() {
   // store data inside of state
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
   // error handling state
   const [error, setError] = useState(null);
   // Fetch Data from https://api.disneyapi.dev/character
 
   async function fetchCharacters() {
+    // attempt to do this...
     try {
       const response = await fetch("https://api.disneyapi.dev/character");
       if (!response.ok) {
         throw new Error(`HTTP Error! Status: ${response.status}`);
       }
       const data = await response.json();
+      console.log(data.info);
       setCharacters(data.data);
+      // if it fails, do this
     } catch (error) {
       setError(error.message);
+      // no matter what, do this
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -27,6 +34,14 @@ export default function Page() {
   }, []);
 
   // if the request is bad, return this
+  // TODO: Extract into a component
+  if (loading) {
+    return (
+      <div className="mx-8">
+        <p className="text-xl">loading...</p>
+      </div>
+    );
+  }
   if (error) {
     return (
       <div className="bg-red-500 flex justify-center items-center p-8">
@@ -38,6 +53,13 @@ export default function Page() {
       </div>
     );
   }
+  if (characters.length === 0) {
+    return (
+      <div className="mx-8">
+        <p className="text-xl font-bold">No Characters. Sorry</p>
+      </div>
+    );
+  }
   return (
     <main className="mx-8">
       <header>
@@ -45,11 +67,9 @@ export default function Page() {
       </header>
       <div>
         <ul>
-          {characters.length > 0
-            ? characters.map((character) => (
-                <li key={character._id}>{character.name}</li>
-              ))
-            : "there are no characters"}
+          {characters.map((character) => (
+            <li key={character._id}>{character.name}</li>
+          ))}
         </ul>
       </div>
     </main>
