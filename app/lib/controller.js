9bcb90d01c8e5b165362d554e81c0d1c9c5d785d
Ashlyn Knox <ashx3s@tuta.io>
2026-03-18 09:51:09 -0600

    added database controller functions

---
+++ app/lib/controller.js
@@ -0,0 +1,27 @@
+// imports: read all of the blog_posts, read a single blog_post, write a new blog_post, update a blog_post, and delete a blog_post
+import { collection, addDoc, getDocs } from "firebase/firestore";
+// import our db from our config
+import { db } from "@/app/lib/firebase/config";
+
+// add Document / add new blog Post item
+
+// take a collection and the data from the user (created in a form) and send it to firestore
+export const addItem = async (collectionName = "blog_posts", data) => {
+  try {
+    await addDoc(collection(db, collectionName), data);
+  } catch (err) {
+    console.error(`Error adding ${data} to ${collection}`);
+  }
+};
+
+export const getItems = async (collectionName) => {
+  try {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    return querySnapshot.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error reading collection: ", collectionName);
+  }
+};
