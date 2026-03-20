f42dcea8bfa407944518701d858dcef1daa21040
Ashlyn Knox <ashx3s@tuta.io>
2026-03-20 08:51:40 -0600

    get items with firestore hook

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -9,6 +9,12 @@ import { useFirestoreCollection } from "../hooks/useFirstoreCollection";
 export default function Page() {
   const { data: items, isLoading, error } = useFirestoreCollection();
   console.log(items);
+  if (isLoading) {
+    return <p>Data Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
     <main>
       <header>
@@ -23,6 +29,13 @@ export default function Page() {
         <h2>Place to display all the posts in the collection</h2>
         <div>
           {/* get all the items and iterate */}
+          {items.map((item) => {
+            return (
+              <article key={item.id}>
+                <h3>{item.title}</h3>
+              </article>
+            );
+          })}
           {/* print out a card for each one */}
           {/* display title, description, author */}
         </div>
