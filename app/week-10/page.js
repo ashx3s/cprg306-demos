09f8672244ba47aa6389537a6bdc8f792603f3fa
Ashlyn Knox <ashx3s@tuta.io>
2026-03-20 10:02:25 -0600

    form updates week 10 to be removed

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -6,9 +6,29 @@ import { useAuth } from "../contexts/AuthContext";
 import { useFirestoreCollection } from "../hooks/useFirstoreCollection";
 // import controller functions addItem and getItems
 
+const formDataSchema = {
+  title: "",
+  description: "",
+  author: "",
+};
+
 export default function Page() {
+  const { authUser, loading } = useAuth();
+  const [formData, setFormData] = useState(formDataSchema);
+
   const { data: items, isLoading, error } = useFirestoreCollection();
-  console.log(items);
+
+  const handleSubmit = async (e) => {
+    // e?.preventDefault()
+    if (!formData.title.trim()) return;
+    const { data } = { ...formData };
+    try {
+      await addItem("blog_posts", data);
+    } catch (error) {
+      console.error(`Error adding ${data}`, error);
+    }
+  };
+
   if (isLoading) {
     return <p>Data Loading...</p>;
   }
@@ -23,12 +43,50 @@ export default function Page() {
       <section>
         <h2>Form to create a new post</h2>
         {/* form element on submit run addItem controller function */}
+        <form onSubmit={handleSubmit}>
+          <div className="my-2">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              value={formData.title}
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+              className="border-2 block my-2"
+            />
+          </div>
+          <div className="my-2">
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              value={formData.author}
+              onChange={(e) =>
+                setFormData({ ...formData, author: e.target.value })
+              }
+              className="border-2 block my-2"
+            />
+          </div>
+          <div className="my-2">
+            <label htmlFor="description"></label>
+            <textarea
+              name="description"
+              id="description"
+              value={formData.description}
+              onChange={(e) =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+              className="border-2 block my-2"
+            ></textarea>
+          </div>
+          <button type="submit" className="bg-blue-500 px-4">
+            Submit
+          </button>
+        </form>
         {/* fields: title, description, author */}
       </section>
       <section>
         <h2>Place to display all the posts in the collection</h2>
         <div>
-          {/* get all the items and iterate */}
           {items.map((item) => {
             return (
               <article key={item.id}>
@@ -36,8 +94,6 @@ export default function Page() {
               </article>
             );
           })}
-          {/* print out a card for each one */}
-          {/* display title, description, author */}
         </div>
       </section>
     </main>
